ef1d32d8b9cbdd191caa3885d76defba479675fa
Tyler <tjb459@nyu.edu>
2019-04-10 15:11:24 -0400

    edited polymorphic relations in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_160554) do
+ActiveRecord::Schema.define(version: 2019_04_10_172011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,11 +39,10 @@ ActiveRecord::Schema.define(version: 2019_04_10_160554) do
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "#<ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition"
     t.integer "content_id"
     t.string "content_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["content_id"], name: "index_comments_on_content_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
