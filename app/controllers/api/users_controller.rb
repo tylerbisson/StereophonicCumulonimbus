9be1a0a42e0b0fe68d1ea6ec6a6b559bc1475036
Tyler <tjb459@nyu.edu>
2019-05-09 12:26:00 -0400

    edit signup error message

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,8 @@ class Api::UsersController < ApplicationController
       login(@user)
       render "api/users/show"
     else
-      render json: @user.errors.full_messages, status: 422
+      # render json: @user.errors.full_messages, status: 422
+      render json: ["Must submit profile image, username, and password with 6 or more characters"], status: 422
     end
   end
 
